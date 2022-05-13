996d6fab670bb1fe850bb3e82b5111c09e251bd0
Mateus <mateusoliveira469@gmail.com>
2022-05-13 19:05:55 -0400

    categorias-mockado

--- src/Category.js
+++ src/Category.js
@@ -4,7 +4,11 @@ export default function Category () {
 
     return (
 
-        <CategoryContainer></CategoryContainer>
+        <CategoryContainer>
+            <CafésEspeciais></CafésEspeciais>
+            <Cafeteiras></Cafeteiras>
+            <XícarasExclusivas></XícarasExclusivas>
+        </CategoryContainer>
 
     );
 
@@ -12,11 +16,56 @@ export default function Category () {
 
 const CategoryContainer = styled.div`
 
-    margin-left: 15px;
+    margin-left: auto;
+    margin-right: auto;
     margin-top: 70px;    
 
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 15px;
+
     height: 120px;
-    width: 120px;
+    width: fit-content;
+
+    background-color: cian;
+
+`;
+
+const CafésEspeciais = styled.div`
+
+    margin-top: 70px;    
+
+    height: 105px;
+    width: 105px;
+
+    background-color: orange;
+    border-radius: 25px;
+
+    z-index: 1;
+
+`;
+
+const Cafeteiras = styled.div`
+
+    margin-top: 70px;    
+
+    height: 105px;
+    width: 105px;
+
+    background-color: orange;
+    border-radius: 25px;
+
+    z-index: 1;
+
+`;
+
+const XícarasExclusivas = styled.div`
+
+    margin-top: 70px;    
+
+    height: 105px;
+    width: 105px;
 
     background-color: orange;
     border-radius: 25px;
